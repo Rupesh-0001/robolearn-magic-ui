65761e8d4e14bd20cad9cdff880add737a4b9e2d
hemant714 <garghemant654@gmail.com>
2025-05-04 01:58:05 +0530

    add reviews section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Hero } from "@/components/sections/Hero";
 import { Features } from "@/components/sections/Features";
 import { CTA } from "@/components/sections/CTA";
+import { Reviews } from "@/components/sections/Reviews"
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Reviews />
       <CTA />
       {/* Add more sections as needed */}
     </>
