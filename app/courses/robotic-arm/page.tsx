ca0615a1ad05b4888ed4cb19b830961849556d75
hemant714 <garghemant654@gmail.com>
2025-05-19 08:30:21 +0530

    improve styling in bootcamp page

--- app/courses/robotic-arm/page.tsx
+++ app/courses/robotic-arm/page.tsx
@@ -1,7 +1,7 @@
 export default function RoboticArmMasterclass() {
     return (
         <div className="container mx-auto px-4 pt-16 mt-6 2xl:pb-8 pb-10">
-            <span className="text-4xl font-bold">Robotic Arm Masterclass</span>
+            <span className="text-4xl font-bold">Robotic Arm Bootcamp</span>
             <br/>
             <span className="text-gray-500">Content Coming Soon</span>
         </div>
