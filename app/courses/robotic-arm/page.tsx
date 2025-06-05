725992f18a05b193a44c5c4ea03879c5d8673035
Rupesh <rupeshahuja28@gmail.com>
2025-06-05 19:17:05 +0530

    Z-index issue resolved

--- app/courses/robotic-arm/page.tsx
+++ app/courses/robotic-arm/page.tsx
@@ -899,7 +899,7 @@ export default function RoboticsEngineeringBootcamp() {
         </div>
       </div>
 
-      <div className="lg:hidden fixed bottom-0 left-0 right-0">
+      <div className="lg:hidden fixed bottom-0 left-0 right-0 z-20">
         <div className="bg-white shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]">
           <div className="h-6 bg-[#fae3ea] mb-3 flex items-center justify-center">
             <div className="text-sm text-[#df4271]">
