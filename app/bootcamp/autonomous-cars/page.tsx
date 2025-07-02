2ef63beebd9cab07485a6ee1966d3149cb56bef3
Rupesh Ahuja <55542613+Rupesh-0001@users.noreply.github.com>
2025-07-02 23:35:26 +0530

    Update page.tsx

--- app/bootcamp/autonomous-cars/page.tsx
+++ app/bootcamp/autonomous-cars/page.tsx
@@ -70,7 +70,7 @@ export default function AutonomousCarMasterclass() {
     }
   };
   useEffect(() => {
-    const endDate = new Date("2025-07-02T23:59:59");
+    const endDate = new Date("2025-07-03T11:59:59");
 
     const calculateTimeLeft = () => {
       const now = new Date();
