1daa1596badb5c1a466d32d518d45ff5aa39092c
hemant714 <garghemant654@gmail.com>
2025-08-03 12:21:38 +0530

    updated for aug 3

--- app/bootcamp/autonomous-cars/page.tsx
+++ app/bootcamp/autonomous-cars/page.tsx
@@ -39,7 +39,7 @@ export default function AutonomousCarMasterclass() {
 
 
   useEffect(() => {
-    const endDate = new Date("2025-08-02T23:59:59");
+    const endDate = new Date("2025-08-03T23:59:59");
 
     const calculateTimeLeft = () => {
       const now = new Date();
