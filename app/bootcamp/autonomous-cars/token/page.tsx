960fb78293815d957cbba6961e2ada3892e556b1
hemant714 <garghemant654@gmail.com>
2025-12-19 00:24:11 +0530

    chages added

--- app/bootcamp/autonomous-cars/token/page.tsx
+++ app/bootcamp/autonomous-cars/token/page.tsx
@@ -73,7 +73,7 @@ export default function AutonomousCarMasterclass() {
 
 
   useEffect(() => {
-    const endDate = new Date("2025-12-14T15:59:59");
+    const endDate = new Date("2025-12-21T15:59:59");
 
     const calculateTimeLeft = () => {
       const now = new Date();
